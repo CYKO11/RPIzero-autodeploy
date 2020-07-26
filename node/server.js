0e0b834d2d6cda9a0251a5c6902f159c2b03e2b2
liamkrieling <lmk310500@gmail.com>
2020-07-27 01:58:23 +0200

    test2

--- node/server.js
+++ node/server.js
@@ -10,9 +10,7 @@ var pins = {
     "22": new Gpio(22, 'out'),
 }
 
-app.listen(8080, () => {
-    console.log(`Server is running on port: 8080`);
-});
+
 app.use(cors());
 app.use(bodyParser());
 app.use(express.json({limit: '50mb'}));
@@ -25,7 +23,9 @@ app.get('/set/:pin',(req, res) => {
 app.get('/read',(req, res) => {
     res.json({"status": pins[req.params.pin].readSync()});
 })
-
+app.listen(8080, () => {
+    console.log(`Server is running on port: 8080`);
+});
 function gpio_states(gpio){
     return ({
         "17":1,

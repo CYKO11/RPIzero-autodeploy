89c6e6f4778557c6ed6e6480f5f6fb5ff2d3dc60
liamkrieling <lmk310500@gmail.com>
2020-07-27 01:36:04 +0200

    test2

--- node/server.js
+++ node/server.js
@@ -4,39 +4,27 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 var Gpio = require('onoff').Gpio; //require onoff to control GPIO
-var light17 = new Gpio(17, 'out'); //declare GPIO4 an output
-var light27 = new Gpio(27, 'out'); //declare GPIO4 an output
-var light22 = new Gpio(22, 'out'); //declare GPIO4 an output
-var port = 8080;
+var pins = {
+    "17": new Gpio(17, 'out'),
+    "27": new Gpio(27, 'out'),
+    "22": new Gpio(22, 'out'),
+}
 
-app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
-    // LEDPin.writeSync(0);
+app.listen(8080, () => {
+    console.log(`Server is running on port: 8080`);
 });
 app.use(cors());
 app.use(bodyParser());
 app.use(express.json({limit: '50mb'}));
-app.get('/pin17', (req,res) => {
-    if (light17.readSync() == 1)
-        light17.writeSync(0);
-    else
-        light17.writeSync(1);
-    res.json({"state":light17.readSync()})
+
+app.get('/set/:pin/:state',(res,req) => {
+    pins[req.params.pin].writeSync(req.params.state);
+    res.json({"status":"done"});
 })
-app.get('/pin27', (req,res) => {
-    if (light27.readSync() == 1)
-        light27.writeSync(0);
-    else
-        light27.writeSync(1);
-    res.json({"state":light27.readSync()})
+app.get('/read/:pin/:state',(res,req) => {
+    res.json({"status": pins[req.params.pin].readSync()});
 })
-app.get('/pin22', (req,res) => {
-    if (light22.readSync() == 1)
-        light22.writeSync(0);
-    else
-        light22.writeSync(1);
-    res.json({"state":light22.readSync()})
-})
+
 function gpio_states(gpio){
     return ({
         "17":1,

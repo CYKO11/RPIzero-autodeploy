8a8ef8e3505d254f68bfc444af680abaafbdc443
liamkrieling <lmk310500@gmail.com>
2020-07-27 15:04:54 +0200

    hardcoded ip adress

--- node/deploy.sh
+++ node/deploy.sh
@@ -2,6 +2,5 @@
 
 git add .
 git commit -m "$1"
-echo $1
-#git push
-#ssh pi@192.168.8.120 "curl localhost:8080/kill;cd RPIzero-autodeploy; git reset --hard; git pull; cd node; node server.js"
+git push
+ssh pi@192.168.8.120 "curl localhost:8080/kill;cd RPIzero-autodeploy; git reset --hard; git pull; cd node; node server.js"

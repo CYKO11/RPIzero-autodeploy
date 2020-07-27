831f317a98a7128a3e8963bf60834c0a08a2202c
liamkrieling <lmk310500@gmail.com>
2020-07-27 15:09:02 +0200

    hardcoded ip adress

--- node/server.js
+++ node/server.js
@@ -15,7 +15,7 @@ pins["17"].writeSync(1);
 pins["27"].writeSync(1);
 pins["22"].writeSync(1);
 app.use(cors());
-app.use(bodyParser());
+app.use(JSON.urlencoded());
 app.use(express.json({limit: '50mb'}));
 app.get('/set/:pin/:state',(req, res) => {
     pins[req.params.pin].writeSync(parseInt(req.params.state));

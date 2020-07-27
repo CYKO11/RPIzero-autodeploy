21e30bf2b1cbb766d10a304735603431ba6fdad8
liamkrieling <lmk310500@gmail.com>
2020-07-27 03:22:46 +0200

    test2

--- node/server.js
+++ node/server.js
@@ -3,15 +3,15 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 const fs = require('fs');
-var Gpio = require('onoff').Gpio;
-var pins = {
-    "17": new Gpio(17, 'out'),
-    "27": new Gpio(27, 'out'),
-    "22": new Gpio(22, 'out'),
-}
-pins["17"].writeSync(1);
-pins["27"].writeSync(1);
-pins["22"].writeSync(1);
+// var Gpio = require('onoff').Gpio;
+// var pins = {
+//     "17": new Gpio(17, 'out'),
+//     "27": new Gpio(27, 'out'),
+//     "22": new Gpio(22, 'out'),
+// }
+// pins["17"].writeSync(1);
+// pins["27"].writeSync(1);
+// pins["22"].writeSync(1);
 app.use(cors());
 app.use(bodyParser());
 app.use(express.json({limit: '50mb'}));

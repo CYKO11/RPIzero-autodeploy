b77eb5e9cdef6c1c0169a15f341e89bceff25d69
liamkrieling <lmk310500@gmail.com>
2020-07-27 02:03:21 +0200

    test2

--- node/server.js
+++ node/server.js
@@ -16,6 +16,7 @@ app.use(cors());
 app.use(bodyParser());
 app.use(express.json({limit: '50mb'}));
 app.get('/set/:pin/:state',(req, res) => {
+    console.log(req.params);
     pins[req.params.pin].writeSync(req.params.state);
     res.json({"status":"done"});
 })

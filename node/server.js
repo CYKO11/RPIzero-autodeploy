1a194fd18f4d0bbed5baf9c779fc3341d6da233f
liamkrieling <lmk310500@gmail.com>
2020-07-27 02:00:28 +0200

    test2

--- node/server.js
+++ node/server.js
@@ -9,27 +9,16 @@ var pins = {
     "27": new Gpio(27, 'out'),
     "22": new Gpio(22, 'out'),
 }
-
-
 app.use(cors());
 app.use(bodyParser());
 app.use(express.json({limit: '50mb'}));
-
-app.get('/set/:pin',(req, res) => {
-    console.log(req.params.pin);
-    // pins[req.params.pin].writeSync(req.params.state);
+app.get('/set/:pin/:state',(req, res) => {
+    pins[req.params.pin].writeSync(req.params.state);
     res.json({"status":"done"});
 })
-app.get('/read',(req, res) => {
+app.get('/read/:pin/:state',(req, res) => {
     res.json({"status": pins[req.params.pin].readSync()});
 })
 app.listen(8080, () => {
     console.log(`Server is running on port: 8080`);
 });
-function gpio_states(gpio){
-    return ({
-        "17":1,
-        "18":0,
-        "19":1
-    });
-}

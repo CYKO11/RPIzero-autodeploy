af036f612f64bc4c0b792fff9f8ba1baaec31ea9
liamkrieling <lmk310500@gmail.com>
2020-07-29 18:11:35 +0200

    full 8 channel support

--- node/server.js
+++ node/server.js
@@ -8,17 +8,27 @@ var pins = {
     "17": new Gpio(17, 'out'),
     "27": new Gpio(27, 'out'),
     "22": new Gpio(22, 'out'),
+    "4": new Gpio(4, 'out'),
+    "18": new Gpio(18, 'out'),
+    "23": new Gpio(23, 'out'),
+    "24": new Gpio(24, 'out'),
+    "25": new Gpio(25, 'out'),
 }
-pins["17"].writeSync(1);
-pins["27"].writeSync(1);
-pins["22"].writeSync(1);
+// server setup
 app.use(cors());
 app.use(bodyParser());
 app.use(express.json({limit: '50mb'}));
+
+// pin state setter
 app.get('/set/:pin/:state',(req, res) => {
-    pins[req.params.pin].writeSync(parseInt(req.params.state));
+    if (req.params.pin === "all")
+        set_all(parseInt(req.params.state));
+    else
+        pins[req.params.pin].writeSync(parseInt(req.params.state));
     res.json({"status":"done"});
 })
+
+// pin state reader
 var pin_keys = []
 var pin_states = {}
 app.get('/read',(req, res) => {
@@ -28,6 +38,8 @@ app.get('/read',(req, res) => {
     })
     res.json(pin_states);
 })
+
+// web page
 app.get('/', (req, res) => {
     fs.readFile('buttons.html', function(err, data) {
         res.writeHead(200, {'Content-Type': 'text/html'});
@@ -35,58 +47,36 @@ app.get('/', (req, res) => {
         return res.end();
     });
 })
+
+// a very usefull function
 function sleep(ms){
     return new Promise(resolve => setTimeout(resolve, ms));
 }
-async function kill_animation(){
-    await sleep(200);
-    pins["17"].writeSync(0);
-    pins["27"].writeSync(0);
-    pins["22"].writeSync(0);
-    await sleep(200);
-    pins["17"].writeSync(1);
-    pins["27"].writeSync(1);
-    pins["22"].writeSync(1);
+
+function set_all(state){
+    pin_keys = Object.keys(pins);
+    pin_keys.forEach((Element) => {
+        pins[Element].writeSync(state);
+    })
 }
-var stop = 0;
-async function blink(ms){
-    await sleep(ms);
-    pins["17"].writeSync(0);
-    await sleep(ms);
-    pins["17"].writeSync(1);
-    if (stop == 0)
-        blink(ms);
-}
-// function surprise(cb) {
-//     (function loop() {
-//         var now = new Date();
-//         console.log([now.getHours(),now.getMinutes()]);
-//         if (now.getHours() === 0 && now.getMinutes() === 22) {
-//             pins["17"].writeSync(0);
-//         }
-//         now = new Date();                  // allow for time passing
-//         var delay = 60000 - (now % 60000); // exact ms to next minute interval
-//         setTimeout(loop, delay);
-//     })();
-// }
-// surprise();
-app.get('/blink/:ms', (req,res) => {
-    if (req.params.ms > 50){
-        stop = 0;
-        blink(parseInt(req.params.ms));
-        res.json({"status":"blinking"});
-    } else res.json({"status":"ms cannot be lower than 50"})
-})
-app.get('/stop', (req,res) => {
-    stop = 1;
-})
+
+// kill command
 app.get('/kill', (req,res) => {
+    async function kill_animation(){
+        await sleep(200);
+        set_all(0);
+        await sleep(200);
+        set_all(1);
+    }
     kill_animation().then(() => {
         console.log('shutdown command recieved');
         res.json({"shutting down":"ready for redeploy"});
         process.exit();
     })
 })
+
+// server host
 app.listen(8080, () => {
     console.log(`Server is running on port: 8080`);
+    set_all(0);
 });
--- node/ssh_deploy.sh
+++ node/ssh_deploy.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
-if [ $1 == "hard" ]
+curl 192.168.8.120:8080/kill
+echo " "
+if [ $1 ]
 then
-    echo "hard_deploy"
-    ssh pi@192.168.8.120 rm -rf package;
-    ssh pi@192.168.8.120 mkdir package;
-    scp -r server.js *.html *.json pi@192.168.8.120:/home/pi/package
-    ssh pi@192.168.8.120 "cd package;npm i;node server.js"
+    if [ $1 == "hard" ]
+    then
+        echo "hard_deploy"
+        ssh pi@192.168.8.120 rm -rf package;
+        ssh pi@192.168.8.120 mkdir package;
+        scp -r server.js *.html *.json node_modules pi@192.168.8.120:/home/pi/package
+        ssh pi@192.168.8.120 "cd package;npm i;node server.js"
+    fi
 else 
     echo "soft_deploy"
     scp -r server.js *.html *.json pi@192.168.8.120:/home/pi/package
     ssh pi@192.168.8.120 "cd package;node server.js"
+    echo "done"
 fi

bc884c7bb6a5cbb830e8d69f0d146346cfd65fd8
liamkrieling <lmk310500@gmail.com>
2020-08-06 11:07:41 +0200

    change script names

--- node/gen_ssh.sh
+++ node/gen_ssh.sh
@@ -1,4 +1,4 @@
-#! bin/bash
+#!/bin/bash
 
 ssh-keygen
 ssh-copy-id -i ~/.ssh/id_rsa pi@192.168.8.120
--- node/server.js
+++ node/server.js
@@ -5,7 +5,6 @@ const app = express();
 const fs = require('fs');
 var Gpio = require('onoff').Gpio;
 var pins = {
-    "17": new Gpio(17, 'out'),
     "27": new Gpio(27, 'out'),
     "22": new Gpio(22, 'out'),
     "4": new Gpio(4, 'out'),
@@ -13,6 +12,7 @@ var pins = {
     "23": new Gpio(23, 'out'),
     "24": new Gpio(24, 'out'),
     "25": new Gpio(25, 'out'),
+    "17": new Gpio(17, 'out'),
 }
 // server setup
 app.use(cors());
@@ -27,12 +27,18 @@ app.get('/set/:pin/:state',(req, res) => {
         pins[req.params.pin].writeSync(parseInt(req.params.state));
     res.json({"status":"done"});
 })
+app.get('/flip/:pin',(req, res) => {
+    if (pins[req.params.pin].readSync() == 1)
+        pins[req.params.pin].writeSync(0);
+    else
+        pins[req.params.pin].writeSync(1);
+    res.json({"status":"done"});
+})
 
 // pin state reader
-var pin_keys = []
+var pin_keys = Object.keys(pins);
 var pin_states = {}
 app.get('/read',(req, res) => {
-    pin_keys = Object.keys(pins);
     pin_keys.forEach((Element) => {
         pin_states[Element] = pins[Element].readSync();
     })
@@ -41,11 +47,26 @@ app.get('/read',(req, res) => {
 
 // web page
 app.get('/', (req, res) => {
-    fs.readFile('buttons.html', function(err, data) {
+    fs.readFile('ui.html', function(err, data) {
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.write(data);
         return res.end();
     });
+})
+
+app.get('/satisfy', (req, res) => {
+    set_all(1);
+    async function satisfy(){
+        await sleep(500);
+        var i = 0;
+        while (i < pin_keys.length){
+            pins[pin_keys[i]].writeSync(0);
+            await sleep(200);
+            i++;
+        }
+    }
+    satisfy();
+    res.json({"status":"are you satisfied"});
 })
 
 // a very usefull function
@@ -54,7 +75,6 @@ function sleep(ms){
 }
 
 function set_all(state){
-    pin_keys = Object.keys(pins);
     pin_keys.forEach((Element) => {
         pins[Element].writeSync(state);
     })
@@ -78,5 +98,5 @@ app.get('/kill', (req,res) => {
 // server host
 app.listen(8080, () => {
     console.log(`Server is running on port: 8080`);
-    set_all(0);
+    // set_all(0);
 });
